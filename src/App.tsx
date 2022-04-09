42db2d01143e02a709ed3e38856cafa02c7f9e7c
Brad <bradtaniguchi@gmail.com>
2022-04-09 17:57:48 +0000

    Fix issue with react-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
-import React from "react";
+import { Link } from "react-router-dom";
 import "./App.css";
 
-function App() {
-  return (
-    <div>
-      <header></header>
-    </div>
-  );
-}
+const App = () => (
+  <div>
+    <header>
+      {/**
+       * Initial version will have basic links on the header
+       *
+       * Later version will have hamburger menu
+       */}
+      <div>
+        {/* Make a link later */}
+        App
+      </div>
+      <Link to="/login">Login</Link>
+      <Link to="/rooms">rooms</Link>
+    </header>
+  </div>
+);
 
 export default App;
