9d8784b5057c8c2d74c01dabac85d35cbe8411cc
Brad <bradtaniguchi@gmail.com>
2022-04-09 19:16:30 +0000

    Squashed commit of the following:
    commit 42e96ffab733cc70ebc2be8da137812c0a2432de
    Author: Brad <bradtaniguchi@gmail.com>
    Date:   Sat Apr 9 18:20:34 2022 +0000
    
        Add pages to be rendered

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,87 @@
+import MenuIcon from "@mui/icons-material/Menu";
+import {
+  AppBar,
+  Button,
+  IconButton,
+  MenuItem,
+  Toolbar,
+  Typography,
+  Box,
+} from "@mui/material";
+import Menu from "@mui/material/Menu";
+import React, { SyntheticEvent, useState } from "react";
 import { Link, Outlet } from "react-router-dom";
 import "./App.css";
 
-const App = () => (
-  <div>
-    <header>
-      {/**
-       * Initial version will have basic links on the header
-       *
-       * Later version will have hamburger menu
-       */}
-      <Link to="/">App</Link>
-      <Link to="/login">Login</Link>
-      <Link to="/rooms">rooms</Link>
-    </header>
-    <main>
-      <Outlet />
-    </main>
-  </div>
-);
+const App = () => {
+  const [anchorElNav, setAnchorElNav] = useState<null | HTMLElement>(null);
+
+  // React.MouseEvent<HTMLElement>
+  const handleOpenNavMenu = (event: SyntheticEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  return (
+    <React.Fragment>
+      <AppBar position="sticky">
+        <Toolbar disableGutters>
+          <IconButton
+            size="large"
+            aria-label="menu"
+            aria-controls="menu-appbar"
+            aria-haspopup="true"
+            onClick={handleOpenNavMenu}
+            color="inherit"
+          >
+            <MenuIcon />
+          </IconButton>
+
+          <Menu
+            id="menu-appbar"
+            anchorEl={anchorElNav}
+            anchorOrigin={{
+              vertical: "bottom",
+              horizontal: "left",
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: "top",
+              horizontal: "left",
+            }}
+            open={Boolean(anchorElNav)}
+            onClose={handleCloseNavMenu}
+          >
+            {[
+              { name: "Login", to: "/login" },
+              { name: "Rooms", to: "/rooms" },
+            ].map(({ name, to }) => (
+              <MenuItem key={name}>
+                <Typography textAlign="center">
+                  <Link onClick={handleCloseNavMenu} to={to}>
+                    {name}
+                  </Link>
+
+                  {/* Add third option for logging out?*/}
+                </Typography>
+              </MenuItem>
+            ))}
+          </Menu>
+
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            <Link to="/">App</Link>
+          </Typography>
+        </Toolbar>
+      </AppBar>
+
+      <Box component="main">
+        <Outlet />
+      </Box>
+    </React.Fragment>
+  );
+};
 
 export default App;
