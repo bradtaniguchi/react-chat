e461c240421c2c216cb9b1b64a0106669240a653
Brad <bradtaniguchi@gmail.com>
2022-04-09 22:56:40 +0000

    Add basics of login UI
    - need to hook into actual changes and do stuff with them.

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,16 +1,55 @@
+import { CardActions } from "@mui/material";
+import Button from "@mui/material/Button";
 import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
 import Grid from "@mui/material/Grid";
+import TextField from "@mui/material/TextField";
+import { useState } from "react";
 
-export const Login = () => (
-  <Grid
-    container
-    spacing={0}
-    direction="column"
-    alignItems="center"
-    style={{ height: "100%" }}
-  >
-    <Card variant="outlined">Card example</Card>
-  </Grid>
-);
+export const Login = () => {
+  const [usernameValue, setUsername] = useState("");
 
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    // TODO: Create user and redirect?
+  };
+
+  const handleUsernameOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUsername((e?.target as HTMLInputElement).value);
+  };
+
+  return (
+    <Grid
+      container
+      spacing={0}
+      direction="column"
+      alignItems="center"
+      style={{ height: "100%" }}
+    >
+      <form onSubmit={handleSubmit}>
+        <Card variant="outlined">
+          <CardContent>
+            <TextField
+              id="username"
+              label="Outlined"
+              variant="outlined"
+              required
+              value={usernameValue}
+              onChange={handleUsernameOnChange}
+            />
+          </CardContent>
+          <CardActions>
+            <Button
+              variant="outlined"
+              type="submit"
+              disabled={!Boolean(usernameValue)}
+            >
+              Login
+            </Button>
+          </CardActions>
+        </Card>
+      </form>
+    </Grid>
+  );
+};
 export default Login;
